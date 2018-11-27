7a5d17fdacd1de43f7c94690a376adbbcf69d7e2
bobo <1055271769@qq.com>
2018-11-27 09:27:50 +0800

    新增弹性形变

---
+++ 医学测试库/测试/弹性形变/弹性形变.py
@@ -0,0 +1,8 @@
+import Augmentor
+p = Augmentor.Pipeline('/home/bobo/data/test/test003')  # 实例化 Pipeline对象  地址为图片路径地址
+# 向管道添加操作
+p.random_distortion(probability=1,grid_width=3,grid_height=3,magnitude=3) # 需要调参
+p.sample(10) # 生成10个样本
+
+
+
