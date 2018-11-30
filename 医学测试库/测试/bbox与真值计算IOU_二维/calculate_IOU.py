60fbb53c512584e27c6e54152b0fc93f8308a04c
bobo <1055271769@qq.com>
2018-11-30 10:15:26 +0800

    修复bug

--- 医学测试库/测试/bbox与真值计算IOU_二维/calculate_IOU.py
+++ 医学测试库/测试/bbox与真值计算IOU_二维/calculate_IOU.py
@@ -4,8 +4,8 @@ import numpy as np
 from IOU import Judge_Much_IOU
 
 
-csv_dir = './node_mark.csv'
-txt_dir = './bounding_box.txt'
+csv_dir = './node_mark.csv' # 有文件头
+txt_dir = './bounding_box.txt'  # 无文件头
 
 
 def load_csv(csv_dir):
@@ -73,7 +73,7 @@ def calculate():
     node_csv = load_csv(csv_dir)
     PA, sum, total = 1, 0, 0
     print('start calculate IOU\nnow is PA1')
-    for line in range(lenjudge_much_IOU(node_csv)):
+    for line in range(len(node_csv)):
         if node_csv.PA[line] != PA:
             PA += 1
             print('now is PA{}'.format(PA))
