d81518015e32614a8eae52642bf778490907e6c0
Administrator <1055271769@qq.com>
2018-12-05 10:41:57 +0800

    结束

--- 医学测试库/测试/预测值与真值计算IOU_三维/calculate_IOU_3D.py
+++ 医学测试库/测试/预测值与真值计算IOU_三维/calculate_IOU_3D.py
@@ -14,20 +14,34 @@ def test():
     # 得到分组的list（ 真值3D块坐标）
     truth_3D = getTruthCoordinate().get_3d_coordinate()
 
-    # 分序列，得到召回率均值(两组序列必须相同)
+    # # 分序列，得到召回率均值(两组序列必须相同)
     value = getRecall(predict_3D,truth_3D).gruop_cal()
     print(value)
 
-    # # 制作数据集
-    #
-    # # 保存 预测3D块 对应标签
-    # # 标准 : 当预测3D块 与真值 立体IOU>0.5，则认定为1
-    # predict_label = getPredictLabel(predict_3D, truth_3D).gruop_cal()
-    #
-    # predict_info=[predict_3D,predict_label]
-    # # 保存 3D坐标
-    # np.save('all_predict_info.npy', predict_info)
-    # np.save('all_truth_3d_coordinate.npy', truth_3D)
+    # 制作数据集
+
+    # 保存 预测3D块 对应标签
+    # 标准 : 当预测3D块 与真值 立体IOU>0.5，则认定为1
+    predict_label = getPredictLabel(predict_3D, truth_3D).gruop_cal()
+
+    predict_info=[predict_3D,predict_label]
+
+
+    temp1 = []
+    temp2 = []
+    # 保存9个完全不想交的预测3D块
+    for i in range(len(predict_3D[0])):
+        if predict_label[0][i] == 0  and len(temp1)<9:
+
+            temp1.append(predict_3D[0][i])
+            temp2.append(0)
+    temp_info = [[temp1],[np.array(temp2)]]
+
+    print()
+
+    # 保存 3D坐标
+    np.save('all_predict_info.npy', temp_info)
+    np.save('all_truth_3d_coordinate.npy', truth_3D)
 
 
 
